c8acd143f41ee7b842f03470323569d2c074c9f7
neo2264 <33mikes66everlasting@gmail.com>
2018-05-19 15:35:01 -0400

    4

--- index.js
+++ index.js
@@ -1,22 +1,15 @@
-const child_process = require('child_process');
-const path = require('path');
 
-process.env.NODE_ENV = process.env.NODE_ENV || 'production';
+function cat(){
+  return 2
+}
+function cat(){
+  return 26
+}
+function cat(){
+  return 25
+}
+function cat(){
+  return 23
+}
+console.log(cat())
 
-const willRun = process.argv.includes('run');
-const willCompile = process.argv.includes('compile') || process.argv.includes('build');
-function cat(){
-  return 2
-}
-function cat(){
-  return 2
-}
-function cat(){
-  return 2
-}
-function cat(){
-  return 2
-}
-console.log(global)
-console.log(global)
-console.log(global)
